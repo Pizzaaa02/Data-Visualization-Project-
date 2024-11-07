ed21ca620c7a4ba2b87eba562b06e3ed11ee9a6c
Grey1128 <109636954+Grey1128@users.noreply.github.com>
2024-11-07 12:52:04 +0800

    Skeleton of website

--- index.js
+++ index.js
@@ -26,3 +26,25 @@ function onClick(element) {
           x.className = x.className.replace(" w3-show", "");
       }
   }
+  window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.fixed-header nav ul li a');
+
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === '#' + current) {
+            link.classList.add('active');
+        }
+    });
+});
